8996677da901ed0451fa0a66960168d34860bf1c
sujandas22 <85474634+sujandas22@users.noreply.github.com>
2021-09-04 11:53:43 -0500

    Update example.py

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ def tokens_lowercase(doc):
     tok = metapy.analyzers.LengthFilter(tok, min=2, max=5)
     tok.set_content(doc.content())
     tokens = [token for token in tok]
-    #print('After filter::',tokens)
+    #print('After filter::::',tokens)
 
     ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
     trigrams = ana.analyze(doc)
